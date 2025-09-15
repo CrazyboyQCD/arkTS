4108e580c8e05f2833058f691318adb9a5eba305
groupguanfang <1203970284@qq.com>
2025-09-16 02:59:07 +0800

    chore: 升级vitest相关依赖并启用monorepo模式，更改vite.config.ts为vite.config.mts以兼容vitest monorepo模式

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,14 +2,18 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
+    projects: ['packages/*'],
     exclude: [
       '**/node_modules/**',
       '**/dist/**',
       '**/out/**',
       '**/cypress/**',
       '**/.{idea,git,cache,output,temp}/**',
-      '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build,eslint,prettier}.config.*',
+      '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build,eslint,prettier,tsdown,uno}.config.*',
       './ohos-typescript/**',
+      'sample/**/*',
+      './packages/vscode/scripts/**',
+      './packages/language-server/bin/**',
     ],
     coverage: {
       exclude: [
@@ -30,10 +34,13 @@ export default defineConfig({
         'test?(-*).?(c|m)[jt]s?(x)',
         '**/*{.,-}{test,spec}?(-d).?(c|m)[jt]s?(x)',
         '**/*__tests__/**',
-        '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
+        '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build,tsdown,uno,eslint}.config.*',
         '**/*vitest.{workspace,projects}.[jt]s?(on)',
         '**/.{eslint,mocha,prettier}rc.{?(c|m)js,yml}',
         './ohos-typescript/**',
+        'sample/**/*',
+        './packages/vscode/scripts/**',
+        './packages/language-server/bin/**',
       ],
     },
   },
