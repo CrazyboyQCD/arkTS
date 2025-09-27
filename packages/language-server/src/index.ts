33bbaafa04f0d66ef4d7dcfe74b45540428eb329
groupguanfang <1203970284@qq.com>
2025-09-27 21:07:22 +0800

    fix(language-server): 添加`ets/onDidChangeTextDocument`事件，修复`onDidChangeTextDocument`钩子不能在lsp启动时监听导致volar提示服务冲突的问题
    volar内部貌似用了onDidChangeTextDocument钩子，该钩子似乎只运行在Initialize时监听一次，不允许多次监听，所以应该实现一个自定义的事件来解决监听TextDocuments的问题。目前已经添加了一个`ets/onDidChangeTextDocument`事件用于监听，vscode侧已经添加了相应的监听逻辑。

--- packages/language-server/src/index.ts
+++ packages/language-server/src/index.ts
@@ -2,8 +2,7 @@ import type { ResourceDiagnosticLevel } from './services/resource-diagnostic.ser
 import process from 'node:process'
 import { ETSLanguagePlugin } from '@arkts/language-plugin'
 import { createArkTServices, createOpenHarmonyProjectDetector } from '@arkts/language-service'
-import { TextDocument } from '@volar/language-server'
-import { createConnection, createServer, createTypeScriptProject } from '@volar/language-server/node'
+import { createConnection, createServer, createTypeScriptProject, TextDocument } from '@volar/language-server/node'
 import * as ets from 'ohos-typescript'
 import { create as createTypeScriptServices } from 'volar-service-typescript'
 import { URI } from 'vscode-uri'
@@ -65,27 +64,22 @@ connection.onInitialize(async (params) => {
   const workspaceDetector = createOpenHarmonyProjectDetector(URI.file(projectRoot))
   const arktsServices = await createArkTServices({ ets, locale: params.locale ?? '' }, workspaceDetector)
   const typescriptServices = createTypeScriptServices(ets as unknown as typeof import('typescript'))
-  const etsLanguagePlugin = ETSLanguagePlugin(ets, {
-    excludePaths: [lspConfiguration.getSdkPath(), lspConfiguration.getHmsSdkPath()].filter(Boolean) as string[],
-    tsdk: lspConfiguration.getTsdkPath(),
-  })
 
   connection.onDidChangeWatchedFiles((params) => {
     for (const change of params.changes)
       workspaceDetector.updateFile(URI.parse(change.uri))
   })
 
-  connection.onDidChangeTextDocument((params) => {
-    for (const change of params.contentChanges) {
-      workspaceDetector.updateTextDocument(
-        TextDocument.create(
-          params.textDocument.uri,
-          etsLanguagePlugin.getLanguageId(params.textDocument.uri) ?? '',
-          params.textDocument.version,
-          change.text,
-        ),
-      )
-    }
+  type SealizableTextDocument = Omit<import('vscode-languageserver-textdocument').TextDocument, 'getText' | 'positionAt' | 'offsetAt' | 'lineCount'> & { text: string }
+  connection.onRequest('ets/onDidChangeTextDocument', (params: { textDocument: SealizableTextDocument }) => {
+    workspaceDetector.updateTextDocument(
+      TextDocument.create(
+        params.textDocument.uri,
+        params.textDocument.languageId,
+        params.textDocument.version,
+        params.textDocument.text,
+      ),
+    )
   })
 
   return server.initialize(
